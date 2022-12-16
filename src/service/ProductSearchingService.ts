b5918b7ce09362b999e9051cac45f596117e0436
Kamil Kubeł <k.kubel@capitalservice.pl>
2022-12-16 08:08:27 +0100

    Fix: Whitespaces

--- src/service/ProductSearchingService.ts
+++ src/service/ProductSearchingService.ts
@@ -1,7 +1,7 @@
 import Product from "../interfaces/Product";
 
 export const searchProvidedProducts = (products: Product[], name: string, searchBy: string): Product[] => {
-    if (!name) return [];
+    if (!name.trim()) return [];
 
     // @ts-ignore
     return products.filter((product: Product) => product[searchBy].toString().includes(name))
