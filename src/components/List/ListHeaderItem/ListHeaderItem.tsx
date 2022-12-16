1836854d4ff7878ca1bc0e81e92eb42f04fc87ad
Kamil Kubeł <k.kubel@capitalservice.pl>
2022-12-16 23:45:40 +0100

    Interactive arrow animation

--- src/components/List/ListHeaderItem/ListHeaderItem.tsx
+++ src/components/List/ListHeaderItem/ListHeaderItem.tsx
@@ -14,9 +14,14 @@ const ListHeaderItem: FC<ListHeaderItemProps> = ({
 }): JSX.Element => {
   const sortOrder = useRef<boolean>(false);
 
-  const sort = (): void => {
+  const sort = (e: any): void => {
     sortFunction(sortBy, !sortOrder.current);
     sortOrder.current = !sortOrder.current;
+    turnAroundArrow(e.target as HTMLElement);
+  };
+
+  const turnAroundArrow = (element: HTMLElement): void => {
+    element.classList.toggle("rotate");
   };
 
   return (
@@ -25,14 +30,14 @@ const ListHeaderItem: FC<ListHeaderItemProps> = ({
         {productField}
         <a>
           <svg
-            onClick={() => sort()}
+            onClick={(e) => sort(e)}
             xmlns="http://www.w3.org/2000/svg"
-            className="ml-1 w-3 h-3 pointer"
+            className="ml-1 w-3 h-3 rotate"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 320 512"
           >
-            <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+            <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66" />
           </svg>
         </a>
       </div>
